b99896b5ac9f2aeef4f66d16c1040fa5ce4f9434
Joel Jacob <85126489+joeljacobvarghese@users.noreply.github.com>
2023-07-31 14:58:15 +0530

    Update extract_text.py

--- llm/extract_text.py
+++ llm/extract_text.py
@@ -1,15 +1,16 @@
 from typing import Generator, Tuple, Iterator
 from pathlib import Path
-import requests
-import io
 import logging
 import json
 from tqdm import tqdm
 
-import pdfplumber
-from pdfplumber.page import Page
-from pdfplumber.pdf import PDF
-from memory_profiler import profile
+import odf
+from odf import text
+from odf.opendocument import load
+
+import sys
+
+sys.path.append("/content/llm-tolkien")
 
 from llm import config
 
@@ -18,59 +19,41 @@ logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 
-def extract(url: str, start_page: int, end_page: int, 
-            header_height: int, footer_height: int, 
-            extraction_path: Path) -> None:
-    """Extract text from a PDF url.
-    Pages are exported in a jsonl file.
+def extract_odt(file_path: Path) -> str:
+    """Extract text from an ODT file.
 
     Args:
-        url (str): url of the pdf
-        start_page (int): first page to consider
-        end_page (int): last page to consider
-        header_height (int): header height in pixels
-        footer_height (int): footer height in pixels
+        file_path (Path): Path to the ODT file.
+
+    Returns:
+        str: Extracted text from the ODT file.
     """
-    LOGGER.info(f'Start extracting pages from {url}')
-    response = requests.get(url)
-    content = io.BytesIO(response.content)
-    with pdfplumber.open(content) as pdf:
-        pages = extract_text_from_pdf(pdf, start_page, end_page, header_height, footer_height)
-    LOGGER.info(f'Finished extracting texts from {url}')
-    to_jsonl(pages=pages, path=extraction_path)
+    LOGGER.info(f"Extracting text from {file_path}")
+    text_content = []
+    doc = load(file_path)
+    for para in doc.getElementsByType(text.P):
+        if para.firstChild is not None:
+            if para.firstChild.__class__.__name__ == "Text":
+                text_content.append(para.firstChild.data)
+
+    return "\n".join(text_content)
 
 
-def to_jsonl(pages: Iterator[Tuple[int, str]], path: Path) -> None:
-    LOGGER.info(f'Start writing to {path}')
+def to_jsonl(text_content: str, path: Path) -> None:
+    LOGGER.info(f"Start writing to {path}")
+    # Print the extracted text before writing it to the JSONL file
+    print(text_content)
+
     # We append text to the existing file with "a" mode (append)
     with open(path, 'a') as f:
-        for page_number, text in tqdm(pages):    
-            dict_page = {page_number: text}
-            json.dump(dict_page, f)
-            f.write('\n')
-    LOGGER.info(f'Finished writing to {path}')
+        dict_page = {"1": text_content}
+        json.dump(dict_page, f)
+        f.write('\n')
+    LOGGER.info(f"Finished writing to {path}")
 
-
-def extract_cropped_text_from_page(page: Page, header_height: int, footer_height: int) -> str:
-    bbox = (0, header_height, page.width, footer_height) # Top-left corner, bottom-right corner
-    text = page.crop(bbox=bbox).extract_text()
-    return text
-
-
-def extract_text_from_pdf(pdf: PDF, start_page: int, end_page: int, 
-                          header_height: int, footer_height: int) -> Generator[Tuple[int, str], None, None]:
-    for page in tqdm(pdf.pages):
-        if page.page_number >= start_page and page.page_number <= end_page:
-            yield page.page_number, extract_cropped_text_from_page(page=page, header_height=header_height, 
-                                                                   footer_height=footer_height)
-            # By default, pdfplumber keeps in cache to avoid to reprocess the same page, leading to memory issues.
-            page.flush_cache()
 
 
 if __name__ == "__main__":
-    extract(url=config.url, 
-            start_page=config.start_page, 
-            end_page=config.end_page, 
-            header_height=config.header_height, 
-            footer_height=config.footer_height,
-            extraction_path=config.extraction_path)
+    file_path = "/content/llm-tolkien/llm/originalpdf/Test.odt"
+    text_content = extract_odt(Path(file_path))
+    to_jsonl(text_content, config.extraction_path)
