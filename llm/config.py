2aaecf93602553c139a2d90dbf7f389fb2946b03
Joel Jacob <85126489+joeljacobvarghese@users.noreply.github.com>
2023-07-31 14:57:18 +0530

    Update config.py

--- llm/config.py
+++ llm/config.py
@@ -15,7 +15,7 @@ extraction_path = REPO_DIR / "llm/data/extracted_text.jsonl"
 min_length = 100
 
 # HF repo
-hf_repo = "JeremyArancio/llm-tolkien"
+hf_repo = "SniiKz/Test"
 
 # Dataset
 context_length = 2048
